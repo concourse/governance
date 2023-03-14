0ec31a68c1c1c2349a39984f7e87b2e954501ccd
Rui Yang <ruiya@vmware.com>
2023-03-14 17:00:24 -0400

    unpin terraform provider versions
    Signed-off-by: Rui Yang <ruiya@vmware.com>

--- providers.tf
+++ providers.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     github = {
-      source  = "integrations/github"
+      source = "integrations/github"
     }
 
     mailgun = {
-      source  = "wgebis/mailgun"
+      source = "wgebis/mailgun"
     }
   }
 }
